b5698301d2a268cda43927e0be5b3604d7337d29
askYangChuan <641617487@qq.com>
2016-08-08 16:18:45 +0800

    修改数据库信息

--- www/config_default.py
+++ www/config_default.py
@@ -11,9 +11,9 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www-data',
-        'password': 'www-data',
-        'database': 'awesome'
+        'user': 'ijdbs',
+        'password': 'ijjazhang',
+        'database': 'web_test'
     },
     'session': {
         'secret': 'AwEsOmE'
